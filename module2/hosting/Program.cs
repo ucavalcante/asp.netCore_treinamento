13c7e2c507d1d053797ea8d6a375ba70dad8ef51
ucavalcante <ziel7@hotmail.com>
2018-03-17 07:00:46 -0300

    criado projeto console

---
+++ module2/hosting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
