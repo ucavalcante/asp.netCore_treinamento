5b29bfe3785b503e15997f9fee5bfebfdbd912a7
ucavalcante <ziel7@hotmail.com>
2018-03-17 08:47:13 -0300

    Midleware apenas de exemplo

--- module2/hosting/Startup.cs
+++ module2/hosting/Startup.cs
@@ -6,6 +6,11 @@ namespace hosting
     public class Startup
     {
         public void Configure(IApplicationBuilder app){
+            app.Use(async (context, next) => {
+                context.Response.Headers.Add("Midleware","Apreendendo");
+                await next.Invoke();
+            });
+
             app.Run(context => context.Response.WriteAsync("Olá Mundo 2"));
         }
     }
