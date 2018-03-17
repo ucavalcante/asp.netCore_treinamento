0efaa6cf2e73879d018dcea2586bb01405ba3059
ucavalcante <ziel7@hotmail.com>
2018-03-17 14:23:06 -0300

    inclusa chamada para a cconfiguração raiz.

--- module2/hosting/Startup.cs
+++ module2/hosting/Startup.cs
@@ -1,10 +1,26 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace hosting
 {
     public class Startup
     {
+        private IConfigurationRoot _configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+         var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)   
+            .AddJsonFile("appsettings.json");
+
+        builder.AddEnvironmentVariables();
+
+        _configuration = builder.Build();
+
+
+        }
         public void Configure(IApplicationBuilder app){
             app.UseMiddleware<MyMiddleware>();
 
