cb249fa9e47ec4be5442a3b59d729cb1ece5aa2b
ucavalcante <ziel7@hotmail.com>
2018-03-22 15:08:06 -0300

    Validação usando ModelState.IsValid

--- module3/Models/Product.cs
+++ module3/Models/Product.cs
@@ -1,10 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+
 namespace module3.Models
 {
     public class Product
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int Price { get; set; }
     }
 }
