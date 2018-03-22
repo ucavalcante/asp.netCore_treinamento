849c7c875b7f5fd42833e4c5af6ba2c18d3c6a5e
ucavalcante <ziel7@hotmail.com>
2018-03-22 13:55:32 -0300

    restringir apenas inteiros.

--- module3/Controllers/ProductController.cs
+++ module3/Controllers/ProductController.cs
@@ -5,7 +5,7 @@ namespace module3.Controllers
     [Route("api/product")]
     public class ProductController
     {
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public int Get(int id){
 
             return id;
