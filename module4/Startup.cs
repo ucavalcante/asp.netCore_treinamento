c5fbcfc3138226a6aad6f0cd60cb56e399ea2cf8
ucavalcante <ziel7@hotmail.com>
2018-03-23 10:34:53 -0300

    Configurada no Startup a string de conexão que será utilizada no EFCore.

--- module4/Startup.cs
+++ module4/Startup.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using module4.Data;
 
 namespace module4
 {
@@ -26,6 +28,10 @@ namespace module4
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("BancoDaAula4"))
+            );
+
             services.AddMvc();
         }
 
