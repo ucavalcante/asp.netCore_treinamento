b149d95e00cc6995b9494b93ea995f08dff44b8c
ucavalcante <ziel7@hotmail.com>
2018-03-24 12:30:33 -0300

    apos ajuste no caminho reexecutado yarn gulp js

--- module6/gulpfile.js
+++ module6/gulpfile.js
@@ -4,7 +4,7 @@ gulp.task('js', function () {
     
     return gulp.src([
         './node_modules/bootstrap/dist/js/bootstrap.min.js',
-        './node_modules/jquery/dist/js/jquery.min.js'
+        './node_modules/jquery/dist/jquery.min.js'
     ]).pipe(gulp.dest('wwwroot/js/'))
     ;
 });
