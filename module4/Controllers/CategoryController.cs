fdbe85a1a79a43ff03e0f43e4f3ea78401a47c8d
ucavalcante <ziel7@hotmail.com>
2018-03-24 01:47:37 -0300

    Implementada injeção de depencia para incluir a categoria no banco.

--- module4/Controllers/CategoryController.cs
+++ module4/Controllers/CategoryController.cs
@@ -1,11 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using module4.Data;
+using module4.Domain;
 
 namespace module4.Controllers
 {
     public class CategoryController: Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
             return View();
         }
     }
