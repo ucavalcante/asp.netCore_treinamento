051efa847c45cd04593d541fbbaa99ada9836893
ucavalcante <ziel7@hotmail.com>
2018-03-24 01:21:00 -0300

    Rollback da Migration ChangeProductAndCategory

--- module4/Data/ApplicationDbContext.cs
+++ module4/Data/ApplicationDbContext.cs
@@ -14,10 +14,7 @@ namespace module4.Data
         public DbSet<Category> Categories { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Product>().ToTable("Prod");
-            modelBuilder.Entity<Product>().Property(p => p.Name).HasMaxLength(100);
-            modelBuilder.Entity<Category>().ToTable("Cat");
-            modelBuilder.Entity<Category>().Property(p => p.Name).HasMaxLength(50);
+           
         }
     }
 }
