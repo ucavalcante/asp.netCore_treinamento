10feb31fccd7ab7fd8940c563d20b64731921e4e
ucavalcante <ziel7@hotmail.com>
2018-03-24 12:24:37 -0300

    yarn gulp js

--- module6/gulpfile.js
+++ module6/gulpfile.js
@@ -2,4 +2,9 @@ var gulp = require('gulp');
 
 gulp.task('js', function () {
     
+    return gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/jquery/dist/js/jquery.min.js'
+    ]).pipe(gulp.dest('wwwroot/js/'))
+    ;
 });
