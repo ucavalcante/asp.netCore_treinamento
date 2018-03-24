9c996d7dd4c86a96b11c5d1ceabc424737bfe711
ucavalcante <ziel7@hotmail.com>
2018-03-24 01:03:13 -0300

    Implementando restrições no banco de dados sem sujar as classes de dominio.

--- module4/Data/ApplicationDbContext.cs
+++ module4/Data/ApplicationDbContext.cs
@@ -12,5 +12,12 @@ namespace module4.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Prod");
+            modelBuilder.Entity<Product>().Property(p => p.Name).HasMaxLength(100);
+            modelBuilder.Entity<Category>().ToTable("Cat");
+            modelBuilder.Entity<Category>().Property(p => p.Name).HasMaxLength(50);
+        }
     }
 }
