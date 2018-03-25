347f8de0f4d7bb693256fd4974f706178b9334d8
ucavalcante <ziel7@hotmail.com>
2018-03-25 18:25:30 -0300

    refeito o curso novamente

---
+++ module6/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('js', function () {
+   
+    return gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/jquery/dist/jquery.min.js'
+    ])
+    .pipe(gulp.dest('wwwroot/js/'));
+});
+
+gulp.task('css',function(){
+
+    return gulp.src([
+        './Styles/site.css',
+        './node_modules/bootstrap/dist/css/bootstrap.css'
+    ])
+    .pipe(concat('site.min.css'))
+    .pipe(gulp.dest('wwwroot/css/'));
+});
