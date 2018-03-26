de4d752d76bf51505707aa1441b4000cb2b880e3
ucavalcante <ziel7@hotmail.com>
2018-03-26 04:28:17 -0300

    Criadas controller e view para a category

---
+++ module7/src/StoreOfBuild.web/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoreOfBuild.web.Models;
+
+namespace StoreOfBuild.web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateOrEdit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateOrEdit(int id)
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
