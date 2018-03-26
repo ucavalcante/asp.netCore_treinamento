6f944440a91d6c1c40ad2436279ce77434f2edb3
ucavalcante <ziel7@hotmail.com>
2018-03-26 00:39:20 -0300

    Implementado Browser Sync

--- module6/gulpfile.js
+++ module6/gulpfile.js
@@ -2,6 +2,16 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
 //var uncss = require('gulp-uncss');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync',function(){
+    browserSync.init({
+        proxy: 'localhost:5000'
+    })
+
+    gulp.watch('./Styles/**/*.css',['css'])
+    gulp.watch('./js/**/*.js',['js'])
+});
 
 gulp.task('js', function () {
    
@@ -10,7 +20,9 @@ gulp.task('js', function () {
         './node_modules/jquery/dist/jquery.min.js',
         './js/site.js'
     ])
-    .pipe(gulp.dest('wwwroot/js/'));
+    .pipe(gulp.dest('wwwroot/js/'))
+    .pipe(browserSync.stream())
+    ;
 });
 
 gulp.task('css',function(){
@@ -22,9 +34,9 @@ gulp.task('css',function(){
     .pipe(concat('site.min.css'))
     .pipe(cssmin())
     //.pipe(uncss({html:['Views/**/*.cshtml']}))
-    .pipe(gulp.dest('wwwroot/css/'));
+    .pipe(gulp.dest('wwwroot/css/'))
+    .pipe(browserSync.stream())
+    ;
 });
 
-gulp.task('watch-css', function(){
-    gulp.watch('./Styles/**/*.css',['css'])
-})
+
--- module6/js/site.js
+++ module6/js/site.js
@@ -1,3 +1,3 @@
 $(function(){
-    alert('olá Usuario')
+    //alert('olá Usuario')
 })
