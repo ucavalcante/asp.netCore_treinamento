2c044df0c193f7116ac418d78fee931ec26445a1
ucavalcante <ziel7@hotmail.com>
2018-03-26 03:05:40 -0300

    Implementada logica de persistencia.

--- module7/src/StoreOfBuild.Domain/Products/ProductStorer.cs
+++ module7/src/StoreOfBuild.Domain/Products/ProductStorer.cs
@@ -12,11 +12,22 @@ namespace StoreOfBuild.Domain.Products
             _productRepository = productRepository;
             _categoryRepository = categoryRepository;
         }
-
-
+        
         public void Store(ProductDto dto)
         {
+            var category = _categoryRepository.GetById(dto.CategoryID);
+            DomainException.When(category == null, "Category Invalid");
 
+            var product = _productRepository.GetById(dto.Id);
+            if (product == null)
+            {
+                product = new Product(dto.Name,category, dto.Price, dto.StockQuantity);
+                _productRepository.Save(product);
+            }
+            else
+            {
+                product.Update(dto.Name,category, dto.Price, dto.StockQuantity);
+            }
         }
     }
 }
