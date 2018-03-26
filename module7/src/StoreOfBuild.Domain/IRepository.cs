d91d124b232c4893371639c9b52b61d3d895fda5
ucavalcante <ziel7@hotmail.com>
2018-03-26 02:57:16 -0300

    Correçoes de sintaxe

--- module7/src/StoreOfBuild.Domain/IRepository.cs
+++ module7/src/StoreOfBuild.Domain/IRepository.cs
@@ -1,7 +1,8 @@
 namespace StoreOfBuild.Domain
 {
-    public interface IRepository
+    public interface IRepository<TEntity>
     {
         TEntity GetById(int id);
         void Save(TEntity entity);
     }
+}
