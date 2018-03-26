987c0fc18ce635ad32999912da34efa3c7ad33aa
ucavalcante <ziel7@hotmail.com>
2018-03-26 00:19:11 -0300

    Incluir alert no site.js para testes.

--- module6/gulpfile.js
+++ module6/gulpfile.js
@@ -7,7 +7,8 @@ gulp.task('js', function () {
    
     return gulp.src([
         './node_modules/bootstrap/dist/js/bootstrap.min.js',
-        './node_modules/jquery/dist/jquery.min.js'
+        './node_modules/jquery/dist/jquery.min.js',
+        './js/site.js'
     ])
     .pipe(gulp.dest('wwwroot/js/'));
 });
