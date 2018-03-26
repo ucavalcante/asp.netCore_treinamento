ed7a54785ac9b2de0e0a65858fd6c60bbb455937
ucavalcante <ziel7@hotmail.com>
2018-03-26 03:57:06 -0300

    Preparando arquivos css e js

---
+++ module7/src/StoreOfBuild.web/gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+//var uncss = require('gulp-uncss');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync',function(){
+    browserSync.init({
+        proxy: 'localhost:5000'
+    })
+
+    gulp.watch('./Styles/**/*.css',['css'])
+    gulp.watch('./js/**/*.js',['js'])
+});
+
+gulp.task('js', function () {
+   
+    return gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/jquery/dist/jquery.min.js',
+        './node_modules/jquery-validation/dist/jquery.validate.min.js',
+        './node_modules/jquery-validation-unobtrusive/node_modules/jquery-validation',
+        './js/site.js'
+    ])
+    .pipe(gulp.dest('wwwroot/js/'))
+    .pipe(browserSync.stream())
+    ;
+});
+
+gulp.task('css',function(){
+
+    return gulp.src([
+        './Styles/site.css',
+        './node_modules/bootstrap/dist/css/bootstrap.css'
+    ])
+    .pipe(concat('site.min.css'))
+    .pipe(cssmin())
+    //.pipe(uncss({html:['Views/**/*.cshtml']}))
+    .pipe(gulp.dest('wwwroot/css/'))
+    .pipe(browserSync.stream())
+    ;
+});
+
+
