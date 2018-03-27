21274fa4e68c498d37931658cb41f0029231bc38
ucavalcante <ziel7@hotmail.com>
2018-03-27 10:09:03 -0300

    Correção do nome do objeto.

--- module7/src/StoreOfBuild.web/Startup.cs
+++ module7/src/StoreOfBuild.web/Startup.cs
@@ -22,7 +22,7 @@ namespace StoreOfBuild.web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            Bootstrap.Configure(services, configuration.GetConnectionSting("DefaultConnection"));
+            Bootstrap.Configure(services, Configuration.GetConnectionString("DefaultConnection"));
             services.AddMvc();
         }
 
