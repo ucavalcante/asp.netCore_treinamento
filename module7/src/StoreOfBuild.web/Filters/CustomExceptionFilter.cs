8bc6eafecd1b168bad9b97e9477c2e5daa13c558
ucavalcante <ziel7@hotmail.com>
2018-03-27 13:46:02 -0300

    Criação Custom Filter

---
+++ module7/src/StoreOfBuild.web/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using StoreOfBuild.Domain;
+
+namespace StoreOfBuild.web.Filters
+{
+    public class CustomExceptionFilter :ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            bool isAjaxCall = context.HttpContext.Request.Headers["x-requested-with"] == "XMLHttpRequest";
+
+            if (isAjaxCall)
+            {
+                context.HttpContext.Response.ContentType = "application/json";
+                context.HttpContext.Response.StatusCode = 500;
+                var message = context.Exception is DomainException? context.Exception.Message : "An error ocurred";
+                context.Result = new JsonResult(message);
+                context.ExceptionHandled = true;
+            }
+
+            base.OnException(context);
+        }
+    }
+}
