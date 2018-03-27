5df49ee781d6676918ea806d26d998a0953de2e2
ucavalcante <ziel7@hotmail.com>
2018-03-27 11:41:28 -0300

    Implmentada interface UniteOfWork

---
+++ module7/src/StoreOfBuild.Data/UnitOfWork.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using StoreOfBuild.Domain;
+
+namespace StoreOfBuild.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
