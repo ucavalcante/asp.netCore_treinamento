0add70dcfba01347680baa4d92c4cf0d91b64e0e
ucavalcante <ziel7@hotmail.com>
2018-03-27 07:53:45 -0300

    Inclusão EntityFrameworkCore.Tools.DotNet

--- module7/src/StoreOfBuild.Data/ApplicationDbContext.cs
+++ module7/src/StoreOfBuild.Data/ApplicationDbContext.cs
@@ -12,6 +12,6 @@ namespace StoreOfBuild.Data
 
         public DbSet<Category> Categories {get; set;}
 
-        public DbSet<Product> Products { get; set; }
+        //public DbSet<Product> Products { get; set; }
     }
 }
