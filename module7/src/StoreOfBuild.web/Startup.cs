65a7e5e1f62fd811d0f84f8f130555f22629888c
ucavalcante <ziel7@hotmail.com>
2018-03-27 01:50:08 -0300

    Adicioada implementação para injeçaõ de dependencia.

--- module7/src/StoreOfBuild.web/Startup.cs
+++ module7/src/StoreOfBuild.web/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StoreOfBuild.DI;
 
 namespace StoreOfBuild.web
 {
@@ -21,6 +22,7 @@ namespace StoreOfBuild.web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            Bootstrap.Configure();
             services.AddMvc();
         }
 
