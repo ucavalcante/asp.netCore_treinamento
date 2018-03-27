d6c35d48f65683b2bceb075de72cfd9c77305af1
ucavalcante <ziel7@hotmail.com>
2018-03-27 00:39:38 -0300

    Adicionaldo construtor e propriedades para dbcontext.

--- module7/src/StoreOfBuild.Data/ApplicationDbContext.cs
+++ module7/src/StoreOfBuild.Data/ApplicationDbContext.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using StoreOfBuild.Domain.Products;
+
 namespace StoreOfBuild.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext :DbContext
     {
-        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories {get; set;}
+
+        public DbSet<Product> Products { get; set; }
     }
 }
